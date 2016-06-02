3497dc43e7abdf3400449700b202200cce53ecdd
Jorge Luiz <jorgeltpf@hotmail.com>
2016-06-02 16:01:25 -0300

    - Modificação no config/database para configurar um banco de teste.

--- config/database.php
+++ config/database.php
@@ -67,9 +67,9 @@ return [
 		'pgsql' => [
 			'driver'   => 'pgsql',
 			'host'     => env('DB_HOST', 'localhost'),
-			'database' => env('DB_DATABASE', 'forge'),
-			'username' => env('DB_USERNAME', 'forge'),
-			'password' => env('DB_PASSWORD', ''),
+			'database' => env('DB_DATABASE', 'imob'),
+			'username' => env('DB_USERNAME', 'postgres_user'),
+			'password' => env('DB_PASSWORD', 'password'),
 			'charset'  => 'utf8',
 			'prefix'   => '',
 			'schema'   => 'public',
